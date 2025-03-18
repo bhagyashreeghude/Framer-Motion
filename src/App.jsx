22d24d4a78ee30899555767c99302d8af7c51770
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2025-03-18 13:48:10 +0530

    3.creating a square pattern using this

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,19 @@ import {motion} from 'motion/react'
 
 const App =() =>{
   return (
-    <div>
-      <motion.div 
-      animate ={{
-        x:750,
-        y:200,
-        rotate:270
-      }}
-      transition={{
-        duration:4,
-        delay:2
-      }}
-      className="circle">
+   <div>
+    <motion.div className='box'
+    animate={{
+      x:[0,1000,1000,0,0],
+      y:[0,0,600,600,0]
+    }}
+    transition={{
+      duration:4,
+      delay:2
+    }}>
 
-      </motion.div>
-      <motion.div 
-      className="box"
-      animate={{
-        x:500,
-        rotate:360
-
-      }}
-      transition={{
-        duration:4,
-        delay:2
-      }}
-      >
-
-      </motion.div>
-    </div>
+    </motion.div>
+   </div>
     
 
   )
