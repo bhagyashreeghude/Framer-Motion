0ad6145df340b0c3a2702ce3c511600e81581920
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2025-03-18 14:03:43 +0530

    transition using jerry's Img

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ const App =() =>{
     <motion.div className='box'
     animate={{
       x:[0,1000,1000,0,0],
-      y:[0,0,600,600,0]
+      y:[0,0,600,600,0],
+      rotate:[0,360,0,-360,0 ]
     }}
     transition={{
       duration:4,
@@ -15,6 +16,20 @@ const App =() =>{
     }}>
 
     </motion.div>
+    <motion.img className='img'
+    animate={{
+      x:1000,
+      y:200,
+      rotate:360
+    }}
+    transition={{
+      duration:2,
+      delay:1
+    }}
+    alt='img'
+    src='https://wallpapers.com/images/featured/tom-and-jerry-png-0ujipwkzhzws7c4j.png'>
+
+    </motion.img>
    </div>
     
 
