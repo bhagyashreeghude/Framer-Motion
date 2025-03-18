b49088d208991d83c8a9b3b131d6451f3774d27f
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2025-03-18 13:36:23 +0530

    1.2 animate,Rotate

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,34 @@ const App =() =>{
   return (
     <div>
       <motion.div 
+      animate ={{
+        x:750,
+        y:200,
+        rotate:270
+      }}
+      transition={{
+        duration:4,
+        delay:2
+      }}
+      className="circle">
+
+      </motion.div>
+      <motion.div 
       className="box"
       animate={{
-        x:100
+        x:500,
+        rotate:360
 
+      }}
+      transition={{
+        duration:4,
+        delay:2
       }}
       >
 
-
       </motion.div>
     </div>
+    
 
   )
 }
